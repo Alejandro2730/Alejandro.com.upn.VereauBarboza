0dc89bbe866a4b9a30d8780ef57f026249eed3ba
Alejandro2730 <113143789+Alejandro2730@users.noreply.github.com>
2023-07-06 10:01:33 -0500

    commit 6

--- AlejandrocomupnVereauBarboza/app/src/main/java/com/example/alejandrocomupnvereaubarboza/Service/DuelistaService.java
+++ AlejandrocomupnVereauBarboza/app/src/main/java/com/example/alejandrocomupnvereaubarboza/Service/DuelistaService.java
@@ -1,4 +1,25 @@
 package com.example.alejandrocomupnvereaubarboza.Service;
 
+import java.util.List;
+
 public interface DuelistaService {
+
+    @GET("Carta")
+    Call<List<carta>> getAllUser(@Query("limit") int limit, @Query("page") int page);
+
+    @GET("Carta/{id}")
+    Call<Cuenta> findUser(@Path("id") int id);
+
+    @POST("Carta")
+    Call<Cuenta> create(@Body Cuenta cuenta);
+
+    @PUT("Carta/{id}")
+    Call<Cuenta> update(@Path("id") int id, @Body Cuenta cuenta);
+
+    @DELETE("Carta/{id}")
+    Call<Void> delete(@Path("id") int id);
+
+    @POST("Carta/upload")
+    Call<Void> uploadCuentas(@Body List<Cuenta> cuentas);
+
 }
