524cd0feadbc5c097da89292e1088a869f637d23
Alejandro2730 <113143789+Alejandro2730@users.noreply.github.com>
2023-07-06 09:00:55 -0500

    Commit 4

--- AlejandrocomupnVereauBarboza/app/src/main/java/com/example/alejandrocomupnvereaubarboza/Clases/carta.java
+++ AlejandrocomupnVereauBarboza/app/src/main/java/com/example/alejandrocomupnvereaubarboza/Clases/carta.java
@@ -1,4 +1,42 @@
 package com.example.alejandrocomupnvereaubarboza.Clases;
 
+
+@Entity(tableName = "Carta")
 public class carta {
+
+    @PrimaryKey()
+    public int id;
+
+    public String nombre;
+
+    @ColumnInfo(name = "synced")
+    private boolean synced;
+
+    public Carta() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public boolean isSynced() {
+        return synced;
+    }
+
+    public void setSynced(boolean synced) {
+        this.synced = synced;
+    }
+
 }
